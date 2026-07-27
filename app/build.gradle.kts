3218b776a9bdf5f9839e8151489831078bada9ca
Lazar Erdeljan <48826271+lerdeljan17@users.noreply.github.com>
2026-07-27 17:17:50 +0200

    Bump app version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lazar.hfpmusicrouter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildFeatures {
